620ba75e768c907e2edc49e83949468a953bd6a9
Maria Bolt <mariabolt@Marias-MacBook-Pro.local>
2019-03-01 11:58:29 -0800

    Friday morning work- see details on board

---
+++ ExodusRemake/Assets/Scripts/MosesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MosesController : MonoBehaviour 
+{
+    public float speed = 2;
+
+    // Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //His movement based on arrow keys
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(moveHorizontal, moveVertical, 0f); 
+	}
+}
